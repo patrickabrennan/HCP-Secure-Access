55c88cb3e5dec96b25199e97af97494106cafbf3
Patrick Brennan <patrickabrennan@gmail.com>
2026-03-09 17:11:52 -0400

    Update self-managed-worker.tf

--- worker/self-managed-worker.tf
+++ worker/self-managed-worker.tf
@@ -430,25 +430,25 @@ data "cloudinit_config" "boundary_self-managed_worker" {
 #parameter. The depends_on argument is set to ensure that the networking is establish first
 #and that the boundary_worker resource also completes, to ensure the token is generated first.
 
-resource "aws_instance" "boundary_self_managed_worker" {
-  ami                         = var.aws_ami
-  key_name                    = "sap"
-  instance_type               = "t2.micro"
-  availability_zone           = var.availability_zone
-  user_data_replace_on_change = true
-  user_data_base64            = data.cloudinit_config.boundary_self-managed_worker.rendered
-  subnet_id                   = aws_subnet.boundary_db_demo_subnet.id
-  vpc_security_group_ids      = [aws_security_group.boundary_ingress_worker_ssh.id]
+#resource "aws_instance" "boundary_self_managed_worker" {
+#  ami                         = var.aws_ami
+#  key_name                    = "sap"
+#  instance_type               = "t2.micro"
+#  availability_zone           = var.availability_zone
+#  user_data_replace_on_change = true
+#  user_data_base64            = data.cloudinit_config.boundary_self-managed_worker.rendered
+#  subnet_id                   = aws_subnet.boundary_db_demo_subnet.id
+#  vpc_security_group_ids      = [aws_security_group.boundary_ingress_worker_ssh.id]
 
   # Attach instance profile so the worker can use IMDS credentials (AssumeRole, etc.)
-  iam_instance_profile        = aws_iam_instance_profile.boundary_worker_instance_profile.name
+ # iam_instance_profile        = aws_iam_instance_profile.boundary_worker_instance_profile.name
 
-  tags = {
-    Name = "Boundary Self-Managed Worker"
-  }
+  #tags = {
+   # Name = "Boundary Self-Managed Worker"
+  #}
 
  # lifecycle {
  #   prevent_destroy = true
  # }
-}
+#}
 */
