f123284d82a56766c1c0c8b4663f1729ae4d9f06
Patrick Brennan <patrickabrennan@gmail.com>
2026-03-10 13:32:36 -0400

    Update vault.tf

--- targets/vault.tf
+++ targets/vault.tf
@@ -1,3 +1,39 @@
+############################################
+# Vault KV Mount for RDP credentials
+############################################
+
+resource "vault_mount" "kv_rdp" {
+  path = var.vault_kv_mount_path
+  type = "kv"
+
+  options = {
+    version = "1"
+  }
+}
+
+############################################
+# Windows RDP Administrator secret
+############################################
+
+resource "vault_generic_secret" "rdp_admin" {
+
+  path = "${var.vault_kv_mount_path}/${var.vault_kv_secret_path}"
+
+  data_json = jsonencode({
+    username = "Administrator"
+    password = trimspace(local.admin_password)
+  })
+}
+
+
+
+
+
+
+
+
+
+/*
 resource "vault_mount" "kv_rdp" {
   count       = var.create_kv_mount ? 1 : 0
   path        = var.vault_kv_mount_path   # "kv-rdp"
@@ -15,7 +51,7 @@ resource "vault_generic_secret" "rdp_admin" {
 
   depends_on = [vault_mount.kv_rdp]
 }
-
+*/
 
 
 
