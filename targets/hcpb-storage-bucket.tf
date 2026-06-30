df7f55fd4b7a33bb658c4bced19f65e759424aeb
Patrick Brennan <patrickabrennan@gmail.com>
2026-06-30 14:05:25 -0400

    Update hcpb-storage-bucket.tf

--- targets/hcpb-storage-bucket.tf
+++ targets/hcpb-storage-bucket.tf
@@ -14,3 +14,20 @@
  # depends_on = [aws_s3_bucket.boundary_session_recording_bucket, aws_db_instance.boundary_demo]
 
 #}
+#added 6/30/2026
+resource "boundary_storage_bucket" "boundary_storage_bucket" {
+  name        = "${random_pet.unique_names.id}-session-recording-bucket"
+  description = "S3 bucket for Boundary SSH and RDP session recording"
+
+  scope_id    = "global"
+  plugin_name = "aws"
+
+  bucket_name = data.terraform_remote_state.worker.outputs.boundary_session_recording_bucket_name
+
+  attributes_json = jsonencode({
+    region                      = var.aws_region
+    disable_credential_rotation = true
+  })
+
+  worker_filter = "\"self-managed-aws-worker\" in \"/tags/type\""
+}
