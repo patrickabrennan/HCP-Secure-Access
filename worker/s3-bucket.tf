7eef8b8e35a6dcfc5eafbb7f13d4b52b2a97ab96
Patrick Brennan <patrickabrennan@gmail.com>
2026-06-30 19:21:21 -0400

    Update s3-bucket.tf

--- worker/s3-bucket.tf
+++ worker/s3-bucket.tf
@@ -1,8 +1,11 @@
 resource "aws_s3_bucket" "boundary_session_recording_bucket" {
-  bucket        = "${random_pet.unique_names.id}-bucket"
+  #bucket        = "${random_pet.unique_names.id}-bucket"
+  6/30/2026
+  bucket = var.s3_bucket_name
   force_destroy = true
   tags = {
-    Name        = var.s3_bucket_name_tags
+    #Name        = var.s3_bucket_name_tags
+    Name = var.s3_bucket_name
     Environment = var.s3_bucket_env_tags
     Terraform   = "true"
   }
