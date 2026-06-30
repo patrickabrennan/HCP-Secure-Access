29a81d69a73fe39f445f8bb0dbf5672f60b3bd74
Patrick Brennan <patrickabrennan@gmail.com>
2026-06-30 14:31:46 -0400

    Update boundary-rdp-target-config.tf

--- targets/boundary-rdp-target-config.tf
+++ targets/boundary-rdp-target-config.tf
@@ -7,21 +7,40 @@ resource "boundary_host_set_plugin" "aws_rdp" {
 }
 
 #add 9-25-2025
-resource "boundary_target" "rdp" {
-  type                     = "rdp"   # was: "tcp"
-  name                     = "aws-windows"
-  description              = "AWS Windows Target"
-  egress_worker_filter     = " \"self-managed-aws-worker\" in \"/tags/type\" "
-  #scope_id                 = boundary_scope.project.id
-  scope_id                 = local.project_scope_id
-  #session_connection_limit = 1
-  default_port             = 3389
-  default_client_port      = 3389
-  host_source_ids          = [boundary_host_set_plugin.aws_rdp.id]
+#resource "boundary_target" "rdp" {
+#  type                     = "rdp"   # was: "tcp"
+#  name                     = "aws-windows"
+#  description              = "AWS Windows Target"
+#  egress_worker_filter     = " \"self-managed-aws-worker\" in \"/tags/type\" "
+#  #scope_id                 = boundary_scope.project.id
+#  scope_id                 = local.project_scope_id
+#  #session_connection_limit = 1
+#  default_port             = 3389
+#  default_client_port      = 3389
+#  host_source_ids          = [boundary_host_set_plugin.aws_rdp.id]
 
-  # ADD this for RDP credential injection:
-  # Inject dynamic RDP creds from Vault into the session:
+#  # ADD this for RDP credential injection:
+#  # Inject dynamic RDP creds from Vault into the session:
+#  injected_application_credential_source_ids = [
+#    boundary_credential_library_vault.rdp_vault_creds.id
+#  ]
+#}
+
+resource "boundary_target" "rdp" {
+  type                 = "rdp"
+  name                 = "aws-windows"
+  description          = "AWS Windows Target"
+  egress_worker_filter = " \"self-managed-aws-worker\" in \"/tags/type\" "
+  scope_id             = local.project_scope_id
+
+  default_port        = 3389
+  default_client_port = 3389
+  host_source_ids     = [boundary_host_set_plugin.aws_rdp.id]
+
   injected_application_credential_source_ids = [
     boundary_credential_library_vault.rdp_vault_creds.id
   ]
+
+  enable_session_recording = true
+  storage_bucket_id        = boundary_storage_bucket.boundary_storage_bucket.id
 }
