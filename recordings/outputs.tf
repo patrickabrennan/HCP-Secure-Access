cd0537005e0f326cbf9d5e169fd6ea4ab2a51d16
Patrick Brennan <patrickabrennan@gmail.com>
2026-06-30 22:49:32 -0400

    Update outputs.tf

--- recordings/outputs.tf
+++ recordings/outputs.tf
@@ -1,7 +1,1 @@
-output "boundary_storage_bucket_id" {
-  value = boundary_storage_bucket.boundary_storage_bucket.id
-}
 
-output "boundary_storage_bucket_name" {
-  value = boundary_storage_bucket.boundary_storage_bucket.name
-}
