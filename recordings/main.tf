4b7b1e5c9c6c9fa467bb13ad3378d3896da712d4
Patrick Brennan <patrickabrennan@gmail.com>
2026-06-30 22:05:03 -0400

    Update main.tf

--- recordings/main.tf
+++ recordings/main.tf
@@ -1,1 +1,21 @@
+terraform {
+  required_providers {
+    boundary = {
+      source  = "hashicorp/boundary"
+      version = ">= 1.3.1"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.1"
+    }
+  }
+}
 
+provider "boundary" {
+  addr                   = var.boundary_addr
+  auth_method_id         = var.auth_method_id
+  auth_method_login_name = var.password_auth_method_login_name
+  auth_method_password   = var.password_auth_method_password
+}
+
+resource "random_pet" "unique_names" {}
