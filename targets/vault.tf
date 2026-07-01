6ede71b7f6c8576e2960617336b59cc52b0b8895
Patrick Brennan <patrickabrennan@gmail.com>
2026-07-01 14:42:21 -0400

    Update vault.tf

--- targets/vault.tf
+++ targets/vault.tf
@@ -1,14 +1,3 @@
-#resource "vault_mount" "kv_rdp" {
-#  count       = var.create_kv_mount ? 1 : 0
-#  path        = var.vault_kv_mount_path
-#  type        = "kv"
-#  description = "KV v1 for Boundary RDP credentials (flat username/password)"
-
-#  options = {
-#   version = "1"
-#  }
-#}
-
 resource "vault_mount" "kv_rdp" {
   count = var.create_kv_mount ? 1 : 0
 
@@ -20,9 +9,6 @@ resource "vault_mount" "kv_rdp" {
   }
 }
 
-
-
-
 #Add 7/1/2026
 resource "vault_generic_secret" "rdp_admin" {
   path = "${var.vault_kv_mount_path}/${var.vault_kv_secret_path}"
@@ -30,64 +16,9 @@ resource "vault_generic_secret" "rdp_admin" {
   data_json = jsonencode({
     username = "Administrator"
     password = trimspace(local.admin_password)
-    domain   = "."
   })
 
   depends_on = [
     vault_mount.kv_rdp
   ]
 }
-
-
-/*
-resource "vault_mount" "kv_rdp" {
-  count       = var.create_kv_mount ? 1 : 0
-  path        = var.vault_kv_mount_path   # "kv-rdp"
-  type        = "kv"                      # KV v1
-  description = "KV v1 for Boundary RDP credentials (flat username/password)"
-}
-
-resource "vault_generic_secret" "rdp_admin" {
-  path = "${var.vault_kv_mount_path}/${var.vault_kv_secret_path}" # kv-rdp/boundary/rdp/svc
-
-  data_json = jsonencode({
-    username = "Administrator"
-    password = local.admin_password
-  })
-
-  depends_on = [vault_mount.kv_rdp]
-}
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-# Create a dedicated KV v1 mount just for Boundary RDP injection.
-# This avoids fighting with an existing "kv" mount that is likely KV v2 in HCP Vault.
-
-resource "vault_mount" "kv_rdp" {
-  count       = var.create_kv_mount ? 1 : 0
-  path        = var.vault_kv_mount_path         # set to "kv-rdp"
-  type        = "kv"                            # KV v1
-  description = "KV v1 for Boundary RDP credentials (flat username/password)"
-}
-
-# Write a flat secret (no nested data.data like KV v2)
-resource "vault_generic_secret" "rdp_admin" {
-  path = "${var.vault_kv_mount_path}/${var.vault_kv_secret_path}" # kv-rdp/boundary/rdp/svc
-
-  data_json = jsonencode({
-    username = "Administrator"
-    password = local.admin_password
-  })
-}
-*/
