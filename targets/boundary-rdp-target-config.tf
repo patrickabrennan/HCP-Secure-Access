06184349e63202b516183550f8e4180a4745e87e
Patrick Brennan <patrickabrennan@gmail.com>
2026-06-30 22:15:47 -0400

    Update boundary-rdp-target-config.tf

--- targets/boundary-rdp-target-config.tf
+++ targets/boundary-rdp-target-config.tf
@@ -42,5 +42,6 @@ resource "boundary_target" "rdp" {
   ]
 
   enable_session_recording = true
-  storage_bucket_id        = boundary_storage_bucket.boundary_storage_bucket.id
+  #storage_bucket_id        = boundary_storage_bucket.boundary_storage_bucket.id
+  storage_bucket_id        = data.terraform_remote_state.recordings.outputs.boundary_storage_bucket_id
 }
